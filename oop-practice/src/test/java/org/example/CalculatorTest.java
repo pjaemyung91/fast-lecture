1e118ac914c7b47ea00dea8321edec6a79570919
Jaemyung <p.jaemyung91@gmail.com>
2023-01-17 17:09:31 +0900

    test코드 수정

--- oop-practice/src/test/java/org/example/CalculatorTest.java
+++ oop-practice/src/test/java/org/example/CalculatorTest.java
@@ -2,15 +2,14 @@ package org.example;
 
 import org.example.calculator.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CalculatorTest {
@@ -33,11 +32,11 @@ public class CalculatorTest {
 //        assertThat(result).isEqualTo(-1);
 //    }
 
-    @DisplayName("덧셈 연산을 수행한다.")
+    @DisplayName("사칙연산을 수행한다.")
     @ParameterizedTest
     @MethodSource("formulaAndResult")
-    void calculatorTest(PositiveNumber operand1, String operator, PositiveNumber operand2, int result) {
-        int calculateResult = Calculator.calculate(operand1, operator, operand2);
+    void calculatorTest(int operand1, String operator, int operand2, int result) {
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
         assertThat(calculateResult).isEqualTo(result);
     }
