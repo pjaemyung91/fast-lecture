17814c427e2e9785a996697e5f57816ee0ec4ff2
Jaemyung <p.jaemyung91@gmail.com>
2023-02-02 17:35:59 +0900

    multi thread, thread pool, singleton, basic of servlet

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,10 +9,13 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
     public CustomWebApplicationServer(int port) {
@@ -31,31 +34,34 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
                 /*
+                * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                * */
+                executorService.execute(new ClientRequestHandler(clientSocket));
+                /*
                 * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
                 * */
-                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
+//                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+//                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+//                    DataOutputStream dos = new DataOutputStream(out);
 
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    //"GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1"
-                    if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-
-                    }
-                }
+//                    HttpRequest httpRequest = new HttpRequest(br);
+//
+//                    //"GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1"
+//                    if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
+//                        QueryStrings queryStrings = httpRequest.getQueryStrings();
+//
+//                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+//                        String operator = queryStrings.getValue("operator");
+//                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+//
+//                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+//                        byte[] body = String.valueOf(result).getBytes();
+//
+//                        HttpResponse response = new HttpResponse(dos);
+//                        response.response200Header("application/json", body.length);
+//                        response.responseBody(body);
+//                    }
+//                }
 
             }
         }
